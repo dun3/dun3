35407cf95553ebde0268eb6e4aaff15231c98723
hertkorn <hertkorn@8f62b829-4955-0410-9075-a120f12ed58f>
2008-09-01 08:05:51 +0000

    JavaPuzzler Hamlet
    git-svn-id: https://dun3.googlecode.com/svn@12 8f62b829-4955-0410-9075-a120f12ed58f

---
+++ misc/JavaPuzzler.Hamlet/JavaPuzzler.Hamlet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JavaPuzzler.Hamlet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            bool toBe = (rnd.Next() % 2) == 0;
+            var result = (toBe || !toBe) ? 3 : 1.0f;
+            Console.WriteLine(result + " " + result.GetType().Name); // GetType notwendig, da 3 rauskommt ;)            
+        }
+    }
+}
