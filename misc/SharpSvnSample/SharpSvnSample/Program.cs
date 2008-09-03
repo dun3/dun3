e9f0bc69713682c12d67c7490cfe08d4edd107f8
hertkorn <hertkorn@8f62b829-4955-0410-9075-a120f12ed58f>
2008-09-03 12:52:36 +0000

    Simple SharpSvn sample
    git-svn-id: https://dun3.googlecode.com/svn@14 8f62b829-4955-0410-9075-a120f12ed58f

---
+++ misc/SharpSvnSample/SharpSvnSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpSvn;
+
+namespace SharpSvnSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SvnClient client = new SvnClient())
+            {
+                SvnUpdateResult result;
+                // Checkout the code to the specified directory
+                client.CheckOut(new Uri("https://svn01.bauerverlag.de/svn/dev_net/dev/Beispiele/"), @"C:\test\SharpSvnCheckoutTest", out result);
+
+            }
+        }
+    }
+}
