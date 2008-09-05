769d33e5fb9610b080e9ffc466a0a2ad00870696
hertkorn <hertkorn@8f62b829-4955-0410-9075-a120f12ed58f>
2008-09-05 09:06:49 +0000

    Verbesserung
    git-svn-id: https://dun3.googlecode.com/svn@15 8f62b829-4955-0410-9075-a120f12ed58f

--- misc/JavaPuzzler.Rhymes/JavaPuzzler.Rhymes/Program.cs
+++ misc/JavaPuzzler.Rhymes/JavaPuzzler.Rhymes/Program.cs
@@ -14,10 +14,9 @@ namespace JavaPuzzler.Rhymes
         static void Main(string[] args)
         {
             StringBuilder word = null;
-            switch (rnd.Next(2))
+            switch (rnd.Next(1))
             {
-                case 1: word = new StringBuilder('S'); break; // Does not compile without the break
-                case 2: word = new StringBuilder('D'); break;
+                case 2: word = new StringBuilder('D'); break; // Does not compile without the break
                 default: word = new StringBuilder('M'); break;
             }
             word.Append('i');
@@ -27,4 +26,26 @@ namespace JavaPuzzler.Rhymes
 
         }
     }
+
+    //class Program
+    //{
+    //    private static readonly int UPPER_BOUND = 1;
+    //    private static readonly int EXCLUSIVE_UPPER_BOUND = UPPER_BOUND + 1;
+
+    //    private static Random rnd = new Random();
+    //    static void Main(string[] args)
+    //    {
+    //        string initialization = string.Empty;
+    //        switch (rnd.Next(EXCLUSIVE_UPPER_BOUND))
+    //        {
+    //            case 1: initialization = "D"; break;
+    //            default: initialization = "M"; break;
+    //        }
+    //        StringBuilder word = new StringBuilder(initialization);
+    //        word.Append('i');
+    //        word.Append('c');
+    //        word.Append('h');
+    //        Console.WriteLine(word);
+    //    }
+    //}
 }
