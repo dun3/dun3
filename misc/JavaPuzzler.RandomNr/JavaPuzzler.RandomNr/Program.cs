e39ca6297e0946e117d67d9b6d6d5f872c57cf7e
hertkorn <hertkorn@8f62b829-4955-0410-9075-a120f12ed58f>
2008-09-05 09:20:52 +0000

    JavaPuzzler.RandomNr initial checkin
    git-svn-id: https://dun3.googlecode.com/svn@16 8f62b829-4955-0410-9075-a120f12ed58f

---
+++ misc/JavaPuzzler.RandomNr/JavaPuzzler.RandomNr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JavaPuzzler.RandomNr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int n = 0;
+            for (int i = 0; i < 100000; i++)
+            {
+                int j = rnd.Next();
+                float f = j;
+                if (Math.Round(f) != j)
+                {
+                    n++;
+                }
+            }
+
+            Console.WriteLine(n);
+        }
+    }
+}
