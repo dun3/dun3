0254e2eba27f36fc6c7aa74d7339398db3a07529
hertkorn <hertkorn@8f62b829-4955-0410-9075-a120f12ed58f>
2008-09-10 15:27:18 +0000

    Duck in C#
    git-svn-id: https://dun3.googlecode.com/svn@21 8f62b829-4955-0410-9075-a120f12ed58f

---
+++ misc/Com.Hertkorn.DuckTypingLight/Com.Hertkorn.DuckTypingLight.Test/GettingStartedTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Com.Hertkorn.DuckTypingLight
+{
+    [TestFixture]
+    public class GettingStartedTest
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            var test = new { Value = 1 };
+            var result = CallSimpleDuck(test.AsDuck<ISimpleDuckInterface>());
+        }
+
+        [Test]
+        public void MethodSimulationTest()
+        {
+            var test = new { Value = 1 };
+            var result = CallSimpleDuck(test.AsDuck<ISimpleDuckInterface>());
+        }
+
+        private int CallSimpleDuck(ISimpleDuckInterface duck)
+        {
+            return duck.Value;
+        }
+
+    }
+
+    public interface ISimpleDuckInterface
+    {
+        int Value { get; }
+    }
+
+    public interface IWithMethodsDuckInterface
+    {
+        Func<int, int> AddAndReport { get; }
+        Func<int> IncrementAndReport { get; }
+        Action<int> Add { get; }
+        Action Increment { get; }
+        int Current { get; }
+    }
+}
---
+++ misc/Com.Hertkorn.DuckTypingLight/Com.Hertkorn.DuckTypingLight/Duck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Hertkorn.DuckTypingLight
+{
+    public static class Duck
+    {
+        public static T AsDuck<T>(this object toBeDucked)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
