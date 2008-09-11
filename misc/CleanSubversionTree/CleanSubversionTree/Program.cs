90447c7dc007db3f3a4156daf402d7a8a4620d17
hertkorn <hertkorn@8f62b829-4955-0410-9075-a120f12ed58f>
2008-09-11 09:45:16 +0000

    New Version that supports blacklisting certain relative or absolute directory paths
    git-svn-id: https://dun3.googlecode.com/svn@22 8f62b829-4955-0410-9075-a120f12ed58f

--- misc/CleanSubversionTree/CleanSubversionTree/Program.cs
+++ misc/CleanSubversionTree/CleanSubversionTree/Program.cs
@@ -8,34 +8,118 @@ namespace CleanSubversionTree
     using System.Linq;
     using System.Text;
     using System.IO;
+    using CleanSubversionTree.Properties;
+    using System.Text.RegularExpressions;
 
-    /// <summary>
-    /// bala aaa aaa aaa
-    /// </summary>
     public class Program
     {
-        /// <summary>
-        /// aaaaaaaag gasd asdf as
-        /// </summary>
-        /// <param name="args">hallo ma lacha</param>
-        public static void Main(string[] args)
+        private static Regex m_excludeDirectories;
+
+        static void Main(string[] args)
         {
-            DirectoryInfo di = new DirectoryInfo("c:\\dev");
-            DirectoryInfo[] dis = di.GetDirectories("*", SearchOption.AllDirectories);
-            //// Array.Sort(dis, new ReverseComparer());
-            foreach (var item in dis)
+            try
             {
-                if (!item.FullName.Contains(".svn"))
+                List<string> excludes = new List<string>();
+                excludes.AddRange(args);
+                excludes.AddRange(Settings.Default.DEFAULT_EXCLUDES.OfType<string>());
+
+                BuildExcludeRegex(excludes);
+
+                DirectoryInfo di = new DirectoryInfo(Settings.Default.STARTING_DIRECTORY);
+                if (!di.Exists) { throw new ArgumentException("Directory '" + di.FullName + "' does not exist"); }
+
+                DeleteFilesAndFolders(di);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.ToString());
+            }
+        }
+
+        private static void BuildExcludeRegex(List<string> excludes)
+        {
+            if (excludes.Count > 0)
+            {
+                StringBuilder regexExpression = new StringBuilder();
+                regexExpression.Append("^(");
+
+                foreach (string exclude in excludes)
                 {
-                    ////Console.WriteLine(item.FullName);
-                    foreach (var file in item.GetFiles())
+                    if (!Path.IsPathRooted(exclude))
                     {
-                        ////Console.WriteLine(file.FullName);
-                        file.Delete();
+                        // relative Path
+                        regexExpression.Append(".*");
+                    }
+
+                    regexExpression.Append(Regex.Escape(exclude));
+                    regexExpression.Append("|");
+                }
+
+                regexExpression.RemoveLastCharacter();
+                regexExpression.Append(").*$");
+                m_excludeDirectories = new Regex(regexExpression.ToString(), RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.IgnoreCase);
+            }
+            else
+            {
+                m_excludeDirectories = new Regex("(^a^b)", RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.IgnoreCase); // an always invalid Regex
+            }
+        }
+
+        private static void DeleteFilesAndFolders(DirectoryInfo di)
+        {
+            DeleteFilesAndFolders(di, false);
+        }
+
+        private static void DeleteFilesAndFolders(DirectoryInfo di, bool deleteDi)
+        {
+            if (!m_excludeDirectories.IsMatch(di.FullName))
+            {
+                DeletingFiles(di);
+
+                foreach (var subDir in di.GetDirectories())
+                {
+                    try
+                    {
+                        DeleteFilesAndFolders(subDir, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("   WARNING: " + subDir.FullName + " " + ex.Message);
+                    }
+                }
+
+                if (deleteDi)
+                {
+                    try
+                    {
+                        di.Delete();
+                    }
+                    catch (Exception)
+                    {
+                        // ignore 
                     }
                 }
             }
-            ////Console.Read();
+        }
+
+        private static void DeletingFiles(DirectoryInfo di)
+        {
+            var childFiles = di.GetFiles();
+
+            foreach (var file in childFiles)
+            {
+                try
+                {
+                    file.IsReadOnly = false;
+                    file.Delete();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("   WARNING: " + file.FullName + " " + ex.Message);
+                }
+
+            }
         }
     }
 }
---
+++ misc/CleanSubversionTree/CleanSubversionTree/StringBuilderExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CleanSubversionTree
+{
+    public static class StringBuilderExtension
+    {
+        public static void RemoveLastCharacter(this StringBuilder builder)
+        {
+            builder.Remove(builder.Length - 1, 1); // remove last character
+        }
+    }
+}
