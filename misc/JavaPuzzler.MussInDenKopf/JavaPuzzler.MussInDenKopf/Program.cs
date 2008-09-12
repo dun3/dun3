17ba051c0551948c0e5bdd6c00d6624666e4b0c0
hertkorn <hertkorn@8f62b829-4955-0410-9075-a120f12ed58f>
2008-09-12 12:56:13 +0000

    JavaPuzzler.MussInDenKopf initial Checkin
    git-svn-id: https://dun3.googlecode.com/svn@23 8f62b829-4955-0410-9075-a120f12ed58f

---
+++ misc/JavaPuzzler.MussInDenKopf/JavaPuzzler.MussInDenKopf/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace JavaPuzzler.MussInDenKopf
+{
+    public class Kampagne
+    {
+        public string Text { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList kopf = new ArrayList();
+            kopf.Add("Bin ich schon drinn?");
+            kopf.Add(new Kampagne() { Text = "Is den heid scho Weihnachtn?" });
+            kopf.Add(11880);
+
+            Auswertung(kopf);
+
+            Console.Read();
+        }
+
+        private static void Auswertung(ArrayList kopf)
+        {
+            Console.WriteLine(kopf.Contains("Bin ich schon drinn?"));
+            Console.WriteLine(kopf.Contains(new Kampagne() { Text = "Is den heid scho Weihnachtn?" }));
+            Console.WriteLine(kopf.Contains(11880));
+        }
+    }
+}
