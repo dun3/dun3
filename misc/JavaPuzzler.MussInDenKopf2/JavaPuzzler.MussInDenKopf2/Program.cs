547a0f474b0764f1e289d8c1b88f047a3034929a
hertkorn <hertkorn@8f62b829-4955-0410-9075-a120f12ed58f>
2008-09-26 10:44:42 +0000

    JavaPuzzler.MussInDenKopf2
    git-svn-id: https://dun3.googlecode.com/svn@26 8f62b829-4955-0410-9075-a120f12ed58f

---
+++ misc/JavaPuzzler.MussInDenKopf2/JavaPuzzler.MussInDenKopf2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JavaPuzzler.MussInDenKopf2
+{
+    public class Kampagne
+    {
+        public string Text { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Kampagne k = obj as Kampagne;
+            if (k == null)
+            {
+                return false;
+            }
+
+            return EqualityComparer<string>.Default.Equals(k.Text, this.Text);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<Kampagne, bool> kopf = new Dictionary<Kampagne, bool>();
+            kopf.Add(new Kampagne() { Text = "Is den heid scho Weihnachtn?" }, true);
+
+            Auswertung(kopf);
+
+            Console.Read();
+        }
+
+        private static void Auswertung(Dictionary<Kampagne, bool> kopf)
+        {
+            Console.WriteLine(kopf[new Kampagne() { Text = "Is den heid scho Weihnachtn?" }]);
+        }
+    }
+}
