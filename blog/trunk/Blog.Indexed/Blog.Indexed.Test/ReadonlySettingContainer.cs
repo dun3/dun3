662c376506ad6882f7c6853dfe10d6d330133beb
hertkorn <hertkorn@8f62b829-4955-0410-9075-a120f12ed58f>
2008-12-25 12:44:51 +0000

    copyright
    git-svn-id: https://dun3.googlecode.com/svn@41 8f62b829-4955-0410-9075-a120f12ed58f

--- blog/trunk/Blog.Indexed/Blog.Indexed.Test/ReadonlySettingContainer.cs
+++ blog/trunk/Blog.Indexed/Blog.Indexed.Test/ReadonlySettingContainer.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright: Tobias Hertkorn
+// Blog: http://saftsack.fs.uni-bayreuth.de/~dun3/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
