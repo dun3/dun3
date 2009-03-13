2d6bba8c750801e9a5170b79af199a45bc2cbfe7
hertkorn <hertkorn@8f62b829-4955-0410-9075-a120f12ed58f>
2009-03-13 22:18:52 +0000

    FilterByExample with anonymous objects
    git-svn-id: https://dun3.googlecode.com/svn@61 8f62b829-4955-0410-9075-a120f12ed58f

---
+++ misc/Com.Hertkorn.Framework.FilterByExample/Com.Hertkorn.Framework.FilterByExample.Test/EnumerableAnonymousTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace Com.Hertkorn.Framework.FilterByExample
+{
+    [TestFixture]
+    public class EnumerableAnonymousTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            m_exampleEnumerable = new List<TestClass>()
+            {
+                new TestClass("test0", 1, 2, 6),
+                new TestClass("test0", 2, 3, 7),
+                new TestClass("test0", 3, 4, 8),
+                new TestClass("test1", 4, 3, 9),
+                new TestClass("test1", 5, 3, 9),
+                new TestClass("test1", 6, 3, 9),
+                new TestClass("test1", 7, 3, 9),
+                new TestClass("test1", 8, 3, 9),
+                new TestClass("test0", 9, 3, 9),
+                new TestClass("test0", 9, 3, 9),
+                new TestClass("test0", 9, 3, 9),
+                new TestClass("test0", 9, 3, 9),
+                new TestClass("test0", 9, 3, 9),
+                new TestClass("test1", 10, 3, 9),
+                new TestClass("test0", 11, 3, 9),
+                new TestClass("test1", 12, 3, 9),
+                new TestClass("test1", 13, 3, 9),
+                new TestClass("test0", 14, 3, 9),
+                new TestClass("test1", 15, 3, 9),
+                new TestClass("test1", 16, 3, 9),
+                new TestClass("test0", 17, 3, 9),
+                new TestClass("test0", 18, 3, 9),
+                new TestClass("test1", 19, 3, 9),
+                new TestClass("test0", 10, 3, 9),
+                new TestClass("test1", 11, 3, 9),
+                new TestClass("test1", 12, 3, 9),
+                new TestClass("test0", 13, 3, 9),
+                new TestClass("test1", 14, 3, 9),
+                new TestClass("test1", 15, 3, 9),
+                new TestClass("test0", 16, 3, 9),
+                new TestClass("test1", 17, 3, 9),
+                new TestClass("test1", 18, 3, 9),
+                new TestClass("test0", 19, 3, 9),
+                new TestClass("test1", 20, 3, 9),
+                new TestClass("test0", 11, 3, 9),
+                new TestClass("test0", 12, 3, 9),
+                new TestClass("test1", 13, 3, 9),
+                new TestClass("test1", 14, 3, 9),
+                new TestClass("test0", 15, 3, 9),
+                new TestClass("test1", 16, 3, 9),
+                new TestClass("test0", 17, 3, 9),
+                new TestClass("test1", 18, 3, 9),
+                new TestClass("test1", 19, 3, 9),
+                new TestClass("test0", 30, 3, 9),
+                new TestClass("test0", 11, 3, 9),
+                new TestClass("test1", 12, 3, 9),
+                new TestClass("test0", 13, 3, 9),
+                new TestClass("test1", 14, 3, 9),
+                new TestClass("test1", 15, 3, 9),
+                new TestClass("test1", 16, 3, 9),
+                new TestClass("test0", 17, 3, 9),
+                new TestClass("test1", 18, 3, 9),
+                new TestClass("test0", 19, 3, 9),
+                new TestClass("test1", 40, 3, 9),
+                new TestClass("test1", 11, 3, 9),
+                new TestClass("test0", 12, 3, 9),
+                new TestClass("test1", 13, 3, 9),
+                new TestClass("test0", 14, 3, 9),
+                new TestClass("test0", 15, 3, 9),
+                new TestClass("test1", 16, 3, 9),
+                new TestClass("test0", 17, 3, 9),
+                new TestClass("test1", 18, 3, 9),
+                new TestClass("test0", 19, 3, 9),
+                new TestClass("test1", 50, 3, 9)
+            }.AsEnumerable();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            m_exampleEnumerable = null;
+        }
+
+        private IEnumerable<TestClass> m_exampleEnumerable;
+
+        [Test]
+        public void OnePropertyTest()
+        {
+            var filtered = m_exampleEnumerable.FilterByExample(new { TestString = "test1" }).ToList();
+
+            Assert.That(filtered.Count, Is.EqualTo(m_exampleEnumerable.FilterByExample(new TestClass("test1", 0, 0, 0), x => x.TestInt, x => x.TestLong).Count()));
+        }
+    }
+}
---
+++ misc/Com.Hertkorn.Framework.FilterByExample/Com.Hertkorn.Framework.FilterByExample/EnumerableAnonymous.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Linq.Expressions;
+
+namespace Com.Hertkorn.Framework.FilterByExample
+{
+    public static class EnumerableAnonymous
+    {
+        public static IEnumerable<T> FilterByExample<T>(this IEnumerable<T> source, object example)
+        {
+            Func<object, T, bool> filter = CreateFilter<T>(example);
+
+            foreach (T item in source)
+            {
+                if (filter(example, item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        private static Func<object, T, bool> CreateFilter<T>(object example)
+        {
+            var examplePropertyz = example.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+
+            if (examplePropertyz.Length == 0)
+            {
+                return (x, y) => true;
+            }
+
+            var otherPropertyz = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+
+            var parameterExample = Expression.Parameter(typeof(object), "example");
+            var parameterOther = Expression.Parameter(typeof(T), "other");
+
+            var otherProperty = GetFittingProperty(examplePropertyz[0], otherPropertyz);
+
+            var castExpression = Expression.Convert(parameterExample, example.GetType());
+
+            var expression = Expression.Equal(
+                Expression.MakeMemberAccess(parameterOther, otherProperty),
+                Expression.MakeMemberAccess(castExpression, examplePropertyz[0])
+                );
+
+            for (int i = 1; i < examplePropertyz.Length; i++)
+            {
+                var otherPropertyInner = GetFittingProperty(examplePropertyz[i], otherPropertyz);
+
+                var next = Expression.Equal(
+                    Expression.MakeMemberAccess(parameterOther, otherPropertyInner),
+                    Expression.MakeMemberAccess(castExpression, examplePropertyz[i])
+                    );
+
+                expression = Expression.AndAlso(expression, next);
+            }
+
+            var filter = Expression.Lambda<Func<object, T, bool>>(expression, parameterExample, parameterOther);
+
+            return filter.Compile();
+        }
+
+        private static PropertyInfo GetFittingProperty(PropertyInfo exampleProperty, PropertyInfo[] otherPropertyz)
+        {
+            var filteredPropertyz = (from s in otherPropertyz
+                                     where s.Name == exampleProperty.Name
+                                     select s).ToList();
+
+            if (filteredPropertyz.Count != 1)
+            {
+                throw new ArgumentException();
+            }
+
+            return filteredPropertyz[0];
+        }
+    }
+}
