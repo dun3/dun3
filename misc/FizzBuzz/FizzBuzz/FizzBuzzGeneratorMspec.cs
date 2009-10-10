f92b2643cba07f4682506dc21d3cc41ec64d72c2
hertkorn <hertkorn@8f62b829-4955-0410-9075-a120f12ed58f>
2009-10-10 21:13:02 +0000

    Redid mspec test - now the "It"s are side-effect free as they should be.
    git-svn-id: https://dun3.googlecode.com/svn@88 8f62b829-4955-0410-9075-a120f12ed58f

--- misc/FizzBuzz/FizzBuzz/FizzBuzzGeneratorMspec.cs
+++ misc/FizzBuzz/FizzBuzz/FizzBuzzGeneratorMspec.cs
@@ -9,125 +9,92 @@ using NUnit.Framework.SyntaxHelpers;
 namespace FizzBuzz
 {
     [Subject(typeof(FizzBuzzGenerator))]
-    public class when_generating_the_sequence
+    public class when_generating_the_sequence : with_output_as_list
     {
         Establish context = () =>
         {
-            fizzBuzzGenerator = new FizzBuzzGenerator();
-        };
-
-        Because of = () =>
-        {
-            enumerator = fizzBuzzGenerator.GetEnumerator();
+            FizzBuzzGenerator fizzBuzzGenerator = new FizzBuzzGenerator();
+            output = fizzBuzzGenerator.ToList();
         };
 
         It should_output__1__for_the_first_element = () =>
         {
-            enumerator.MoveNext();
-            enumerator.Current.ShouldEqual("1");
+            output.First().ShouldEqual("1");
         };
 
         It should_output__Buzz__for_the_last_element = () =>
         {
-            string current = null;
-            while (enumerator.MoveNext())
-            {
-                current = enumerator.Current;
-            }
-            current.ShouldEqual("Buzz");
+            output.Last().ShouldEqual("Buzz");
         };
 
         It should_output_something_containing__Fizz__for_any_items_mod_3 = () =>
         {
-            for (int i = 1; enumerator.MoveNext(); i++)
-            {
-                if ((i % 3) == 0)
-                {
-                    Assert.That(enumerator.Current.Contains("Fizz"), Is.True);
-                }
-            }
+            TestForAllItems(i => (i % 3) == 0, item => item.Contains("Fizz").ShouldBeTrue());
         };
 
         It should_not_output_something_containing__Fizz__for_any_items_mod_3 = () =>
         {
-            for (int i = 1; enumerator.MoveNext(); i++)
-            {
-                if ((i % 3) != 0)
-                {
-                    Assert.That(enumerator.Current.Contains("Fizz"), Is.False);
-                }
-            }
+            TestForAllItems(i => (i % 3) != 0, item => item.Contains("Fizz").ShouldBeFalse());
         };
 
         It should_output_something_containing__Buzz__for_any_items_mod_5 = () =>
         {
-            for (int i = 1; enumerator.MoveNext(); i++)
-            {
-                if ((i % 5) == 0)
-                {
-                    Assert.That(enumerator.Current.Contains("Buzz"), Is.True);
-                }
-            }
+            TestForAllItems(i => (i % 5) == 0, item => item.Contains("Buzz").ShouldBeTrue());
         };
 
         It should_not_output_something_containing__Buzz__for_any_items_mod_5 = () =>
         {
-            for (int i = 1; enumerator.MoveNext(); i++)
-            {
-                if ((i % 5) != 0)
-                {
-                    Assert.That(enumerator.Current.Contains("Buzz"), Is.False);
-                }
-            }
+            TestForAllItems(i => (i % 5) != 0, item => item.Contains("Buzz").ShouldBeFalse());
         };
 
         It should_output__FizzBuzz__for_any_items_mod_15 = () =>
         {
-            for (int i = 1; enumerator.MoveNext(); i++)
-            {
-                if ((i % 15) == 0)
-                {
-                    Assert.That(enumerator.Current.Contains("FizzBuzz"), Is.True);
-                }
-            }
+            TestForAllItems(i => (i % 15) == 0, item => item.ShouldEqual("FizzBuzz"));
         };
 
         It should_not_output__FizzBuzz__for_any_items_mod_15 = () =>
         {
-            for (int i = 1; enumerator.MoveNext(); i++)
-            {
-                if ((i % 15) != 0)
-                {
-                    Assert.That(enumerator.Current.Contains("FizzBuzz"), Is.False);
-                }
-            }
+            TestForAllItems(i => (i % 15) != 0, item => item.ShouldNotEqual("FizzBuzz"));
         };
 
         It should_output_numbers_for_any_items_not_mod_3_and_not_mod_5 = () =>
         {
-            for (int i = 1; enumerator.MoveNext(); i++)
-            {
-                if (((i % 3) != 0) && ((i % 5) != 0))
-                {
-                    enumerator.Current.ShouldEqual(i.ToString());
-                }
-            }
+            TestForAllItems(i => ((i % 3) != 0) && ((i % 5) != 0), (i, item) => item.ShouldEqual(i.ToString()));
         };
 
         It should_output_100_items = () =>
         {
-            int i = 0;
-            while (enumerator.MoveNext())
-            {
-                i++;
-            }
-            i.ShouldEqual(100);
+            output.Count.ShouldEqual(100);
         };
 
         Cleanup after_each = () =>
-            enumerator = null;
+            output = null;
+    }
 
-        public static IEnumerator<string> enumerator;
-        private static FizzBuzzGenerator fizzBuzzGenerator;
+    public class with_output_as_list
+    {
+        public static IList<string> output;
+
+        public static void TestForAllItems(Func<int, bool> testThisNumber, Action<string> assert)
+        {
+            for (int i = 1; i <= output.Count; i++)
+            {
+                if (testThisNumber(i))
+                {
+                    assert(output[i - 1]);
+                }
+            }
+        }
+
+        public static void TestForAllItems(Func<int, bool> testThisNumber, Action<int, string> assert)
+        {
+            for (int i = 1; i <= output.Count; i++)
+            {
+                if (testThisNumber(i))
+                {
+                    assert(i, output[i - 1]);
+                }
+            }
+        }
     }
 }
